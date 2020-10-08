23875c915431ac68404cfe0ccade212a36e1361c
Abdi-01 <abdialghi@gmail.com>
2020-10-08 21:25:28 +0700

    Updates

---
+++ precache-manifest.954210d685ba1aa28c3aae08c9b42b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31ea0d53c07f0c0c808e947f419dd2a7",
+    "url": "/covid-react/index.html"
+  },
+  {
+    "revision": "fc90de353ae772896823",
+    "url": "/covid-react/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "110c28a0acccefd8fbe3",
+    "url": "/covid-react/static/css/main.4b94981a.chunk.css"
+  },
+  {
+    "revision": "fc90de353ae772896823",
+    "url": "/covid-react/static/js/2.47629210.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid-react/static/js/2.47629210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "110c28a0acccefd8fbe3",
+    "url": "/covid-react/static/js/main.6e1629bd.chunk.js"
+  },
+  {
+    "revision": "50d69a2dcb8d1e543c18",
+    "url": "/covid-react/static/js/runtime-main.5134be27.js"
+  }
+]);
